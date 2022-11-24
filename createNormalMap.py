2a0263a4de449b2bfead727ac2a9e8fea6350f6f
john-salame <josa5095@colorado.edu>
2022-11-24 00:54:39 -0700

    Final Project Version 1 - Normal Map Generator X Component

--- createNormalMap.py
+++ createNormalMap.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image
 import cv2
 import sys
 
@@ -17,7 +18,7 @@ def convert_white_stripes_to_indent(k, img):
         for j in range(k, w-k):
             if img[i,j] > 200:
                 img[i,j] = np.average(img[i-k:i+k,j-k:j+k]) - 20
-    return img.astype('uint32')
+    return img.astype('uint8')
 
 # if the right side is brighter, normal is to left (negative red). Thus, we subtract pixel intensities on the right and add intensities on the left.
 # the kernel is size k (radius)
@@ -70,10 +71,19 @@ def red_normal(k, img):
         # red_img[:, j+k] = np.sum(np.multiply(kernel, img_pad[:, j:(j+1+2*k)]), axis=1)
         red_img[:, j+k] = np.sum(img_pad[:, j:(j+1+2*k)] * kernel, axis=1)
         # red_img[:, j+k] = np.matmul(img_pad[:, j:(j+1+2*k)], kernel)
+    red_img = red_img[k:(h-k), k:(w-k)]
+    print(red_img.shape)
     print('max red %f' % np.amax(red_img))
     print('min red %f' % np.amin(red_img))
     print('avg red %f' % np.average(red_img))
-    return red_img
+    # shift the mean over to 0.5 and call it a day. It should be fine as long as in and max do not exceet [-0.5, 0.5]
+    my_avg = np.average(red_img)
+    red_img = red_img + 0.5 - my_avg
+    red_img = red_img * 255
+    print('max red %f' % np.amax(red_img))
+    print('min red %f' % np.amin(red_img))
+    print('avg red %f' % np.average(red_img))
+    return red_img.astype('uint8')
 
 def main():
     img = plt.imread(in_file)
@@ -81,23 +91,32 @@ def main():
     print(type(img))
     print(img.shape)
     # keep intensities only (b here stands for brightness)
-    img_b = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint32)
+    img_b = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
     img_b = 0.33*img[:,:,0] + 0.33*img[:,:,1] + 0.33*img[:,:,2]
     # Create a depression where the white slash on the texture is
     if in_file == 'candyCane.bmp':
         img_b = convert_white_stripes_to_indent(k, img_b)
-    img_b = red_normal(k, img_b)
-    print(img_b.shape)
-    print(img_b[150, :])
+    reds = red_normal(k, img_b)
+    # now, create the normal map
+    normal_map = np.full(img.shape, 128, dtype=np.uint8)
+    normal_map[:,:,0] = reds
+    normal_map[:,:,2] = 255 # z should be fully out for all parts
+    # plot the image and normal map side-by-side
     plt.subplot(1, 2, 1)
     plt.imshow(img)
     plt.title('Original texture')
     plt.subplot(1, 2, 2)
     # plt.imshow(img_b, cmap=plt.get_cmap('gray'))
-    plt.imshow(img_b, cmap=plt.colormaps['Reds'])
+    plt.imshow(normal_map)
     plt.colorbar()
-    plt.title('Grayscale')
+    plt.title('Normal Map')
     plt.show()
+    saveyn = input('save image? ')
+    if saveyn.upper() == 'Y':
+        extension_index = in_file.find('.bmp')
+        out_file = in_file[:extension_index] + '_normal' + in_file[extension_index:]
+        print("Saving normal map to file %s" % out_file)
+        matplotlib.image.imsave(out_file, normal_map)
     return 0
 
 if __name__ == "__main__":
